ea41781804afb873e46b253e7ad705151e28fdc3
Stuart Colville <scolville@mozilla.com>
2021-03-22 11:08:42 +0000

    Fix lint

--- tests/fixtures/bz-whiteboard-tags.js
+++ tests/fixtures/bz-whiteboard-tags.js
@@ -4,9 +4,9 @@ export default {
     url:
       'https://bugzilla.mozilla.org/buglist.cgi?bug_status=ASSIGNED&bug_status=NEW&bug_status=REOPENED&bug_status=UNCONFIRMED&component=Add-ons%20Manager&component=Android&component=Compatibility&component=Developer%20Outreach&component=Developer%20Tools&component=Experiments&component=Frontend&component=General&component=Request%20Handling&component=Storage&component=Themes&component=Untriaged&product=Toolkit&product=WebExtensions&resolution=---&status_whiteboard=addons-ux&status_whiteboard_type=allwordssubstr',
   },
-  'prod_bugs': {
-    'count': 4,
-    'url':
+  prod_bugs: {
+    count: 4,
+    url:
       'https://bugzilla.mozilla.org/buglist.cgi?bug_status=ASSIGNED&bug_status=NEW&bug_status=REOPENED&bug_status=UNCONFIRMED&component=Add-ons%20Manager&component=Android&component=Compatibility&component=Developer%20Outreach&component=Developer%20Tools&component=Experiments&component=Frontend&component=General&component=Request%20Handling&component=Storage&component=Themes&component=Untriaged&product=Toolkit&product=WebExtensions&resolution=---&status_whiteboard=prod_bugs&status_whiteboard_type=allwordssubstr',
   },
   'stockwell disable-recommended': {
