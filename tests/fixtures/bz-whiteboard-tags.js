fece8e5346dea09ede87629b101a5aafe52a9395
Stuart Colville <scolville@mozilla.com>
2021-05-13 16:58:40 +0100

    prod_bugs -> prod_bug

--- tests/fixtures/bz-whiteboard-tags.js
+++ tests/fixtures/bz-whiteboard-tags.js
@@ -4,10 +4,10 @@ export default {
     url:
       'https://bugzilla.mozilla.org/buglist.cgi?bug_status=ASSIGNED&bug_status=NEW&bug_status=REOPENED&bug_status=UNCONFIRMED&component=Add-ons%20Manager&component=Android&component=Compatibility&component=Developer%20Outreach&component=Developer%20Tools&component=Experiments&component=Frontend&component=General&component=Request%20Handling&component=Storage&component=Themes&component=Untriaged&product=Toolkit&product=WebExtensions&resolution=---&status_whiteboard=addons-ux&status_whiteboard_type=allwordssubstr',
   },
-  prod_bugs: {
+  prod_bug: {
     count: 4,
     url:
-      'https://bugzilla.mozilla.org/buglist.cgi?bug_status=ASSIGNED&bug_status=NEW&bug_status=REOPENED&bug_status=UNCONFIRMED&component=Add-ons%20Manager&component=Android&component=Compatibility&component=Developer%20Outreach&component=Developer%20Tools&component=Experiments&component=Frontend&component=General&component=Request%20Handling&component=Storage&component=Themes&component=Untriaged&product=Toolkit&product=WebExtensions&resolution=---&status_whiteboard=prod_bugs&status_whiteboard_type=allwordssubstr',
+      'https://bugzilla.mozilla.org/buglist.cgi?bug_status=ASSIGNED&bug_status=NEW&bug_status=REOPENED&bug_status=UNCONFIRMED&component=Add-ons%20Manager&component=Android&component=Compatibility&component=Developer%20Outreach&component=Developer%20Tools&component=Experiments&component=Frontend&component=General&component=Request%20Handling&component=Storage&component=Themes&component=Untriaged&product=Toolkit&product=WebExtensions&resolution=---&status_whiteboard=prod_bug&status_whiteboard_type=allwordssubstr',
   },
   'stockwell': {
     count: 4,
