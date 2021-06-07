9343787bb535d2806f9dcedc81664288370be0d8
Stuart Colville <scolville@mozilla.com>
2021-06-07 19:17:07 +0100

    Update whiteboard tags (#609)

--- tests/fixtures/bz-whiteboard-tags.js
+++ tests/fixtures/bz-whiteboard-tags.js
@@ -11,20 +11,20 @@ export default {
     count: 4,
     url: 'https://bugzilla.mozilla.org/buglist.cgi?bug_status=ASSIGNED&bug_status=NEW&bug_status=REOPENED&bug_status=UNCONFIRMED&component=Add-ons%20Manager&component=Android&component=Compatibility&component=Developer%20Outreach&component=Developer%20Tools&component=Experiments&component=Frontend&component=General&component=Request%20Handling&component=Storage&component=Themes&component=Untriaged&product=Toolkit&product=WebExtensions&resolution=---&status_whiteboard=stockwell&status_whiteboard_type=allwordssubstr',
   },
-  'mv3:m1': {
+  '[mv3-m1]': {
     count: 4,
-    url: 'https://bugzilla.mozilla.org/buglist.cgi?bug_status=ASSIGNED&bug_status=NEW&bug_status=REOPENED&bug_status=UNCONFIRMED&component=Add-ons%20Manager&component=Android&component=Compatibility&component=Developer%20Outreach&component=Developer%20Tools&component=Experiments&component=Frontend&component=General&component=Request%20Handling&component=Storage&component=Themes&component=Untriaged&product=Toolkit&product=WebExtensions&resolution=---&status_whiteboard=mv3%3Am1&status_whiteboard_type=allwordssubstr',
+    url: 'https://bugzilla.mozilla.org/buglist.cgi?bug_status=ASSIGNED&bug_status=NEW&bug_status=REOPENED&bug_status=UNCONFIRMED&component=Add-ons%20Manager&component=Android&component=Compatibility&component=Developer%20Outreach&component=Developer%20Tools&component=Experiments&component=Frontend&component=General&component=Request%20Handling&component=Storage&component=Themes&component=Untriaged&product=Toolkit&product=WebExtensions&resolution=---&status_whiteboard=%5Bmv3-m1%5D&status_whiteboard_type=allwordssubstr',
   },
-  'mv3:m2': {
+  '[mv3-m2]': {
     count: 4,
-    url: 'https://bugzilla.mozilla.org/buglist.cgi?bug_status=ASSIGNED&bug_status=NEW&bug_status=REOPENED&bug_status=UNCONFIRMED&component=Add-ons%20Manager&component=Android&component=Compatibility&component=Developer%20Outreach&component=Developer%20Tools&component=Experiments&component=Frontend&component=General&component=Request%20Handling&component=Storage&component=Themes&component=Untriaged&product=Toolkit&product=WebExtensions&resolution=---&status_whiteboard=mv3%3Am2&status_whiteboard_type=allwordssubstr',
+    url: 'https://bugzilla.mozilla.org/buglist.cgi?bug_status=ASSIGNED&bug_status=NEW&bug_status=REOPENED&bug_status=UNCONFIRMED&component=Add-ons%20Manager&component=Android&component=Compatibility&component=Developer%20Outreach&component=Developer%20Tools&component=Experiments&component=Frontend&component=General&component=Request%20Handling&component=Storage&component=Themes&component=Untriaged&product=Toolkit&product=WebExtensions&resolution=---&status_whiteboard=%5Bmv3-m2%5D&status_whiteboard_type=allwordssubstr',
   },
-  'mv3:m3': {
+  '[mv3-m3]': {
     count: 4,
-    url: 'https://bugzilla.mozilla.org/buglist.cgi?bug_status=ASSIGNED&bug_status=NEW&bug_status=REOPENED&bug_status=UNCONFIRMED&component=Add-ons%20Manager&component=Android&component=Compatibility&component=Developer%20Outreach&component=Developer%20Tools&component=Experiments&component=Frontend&component=General&component=Request%20Handling&component=Storage&component=Themes&component=Untriaged&product=Toolkit&product=WebExtensions&resolution=---&status_whiteboard=mv3%3Am3&status_whiteboard_type=allwordssubstr',
+    url: 'https://bugzilla.mozilla.org/buglist.cgi?bug_status=ASSIGNED&bug_status=NEW&bug_status=REOPENED&bug_status=UNCONFIRMED&component=Add-ons%20Manager&component=Android&component=Compatibility&component=Developer%20Outreach&component=Developer%20Tools&component=Experiments&component=Frontend&component=General&component=Request%20Handling&component=Storage&component=Themes&component=Untriaged&product=Toolkit&product=WebExtensions&resolution=---&status_whiteboard=%5Bmv3-m3%5D&status_whiteboard_type=allwordssubstr',
   },
-  'mv3:future': {
+  '[mv3-future]': {
     count: 4,
-    url: 'https://bugzilla.mozilla.org/buglist.cgi?bug_status=ASSIGNED&bug_status=NEW&bug_status=REOPENED&bug_status=UNCONFIRMED&component=Add-ons%20Manager&component=Android&component=Compatibility&component=Developer%20Outreach&component=Developer%20Tools&component=Experiments&component=Frontend&component=General&component=Request%20Handling&component=Storage&component=Themes&component=Untriaged&product=Toolkit&product=WebExtensions&resolution=---&status_whiteboard=mv3%3Afuture&status_whiteboard_type=allwordssubstr',
+    url: 'https://bugzilla.mozilla.org/buglist.cgi?bug_status=ASSIGNED&bug_status=NEW&bug_status=REOPENED&bug_status=UNCONFIRMED&component=Add-ons%20Manager&component=Android&component=Compatibility&component=Developer%20Outreach&component=Developer%20Tools&component=Experiments&component=Frontend&component=General&component=Request%20Handling&component=Storage&component=Themes&component=Untriaged&product=Toolkit&product=WebExtensions&resolution=---&status_whiteboard=%5Bmv3-future%5D&status_whiteboard_type=allwordssubstr',
   },
 };
