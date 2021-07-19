9c69cf2e57fbb7f053aaf5f17c10461a5e026e6f
Stuart Colville <scolville@mozilla.com>
2021-07-19 19:24:44 +0100

    Fix queries for contrib (#642)

--- pages/api/gh-contrib-welcome.js
+++ pages/api/gh-contrib-welcome.js
@@ -1,19 +1,11 @@
 import gql from 'graphql-tag';
 import createClient from 'lib/ghapi';
 import { contribRepos } from 'lib/const';
+import { oneLine } from 'common-tags';
 
-const query = gql`{
-    contrib_welcome: search(
-      type: ISSUE
-      query: """
-      ${contribRepos.map((n) => `repo:${n}`).join('\n')}
-      label:"contrib: welcome"
-      is:open
-      sort:updated-desc
-      type:issues
-      """
-      first: 100
-    ) {
+const query = gql`
+  query getContribWelcome($query: String!) {
+    contrib_welcome: search(type: ISSUE, query: $query, first: 100) {
       issueCount
       results: edges {
         issue: node {
@@ -39,8 +31,16 @@ const query = gql`{
 
 export default async (req, res) => {
   const client = createClient();
+  const variables = {
+    query: oneLine`${contribRepos.map((n) => `repo:${n}`).join('\n')}
+      label:"contrib: welcome"
+      is:open
+      sort:updated-desc
+      type:issues`,
+  };
   const data = await client.query({
     query,
+    variables,
   });
   res.json(data);
 };
