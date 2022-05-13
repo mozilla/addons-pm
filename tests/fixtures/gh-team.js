96e7f91ba2c2ec47e76db12997ae18ec585bef1b
Mathieu Pillard <mpillard@mozilla.com>
2022-05-13 15:37:53 +0200

    prettier

--- tests/fixtures/gh-team.js
+++ tests/fixtures/gh-team.js
@@ -4,37 +4,37 @@ export default {
       edges: [
         {
           node: {
-            id: "SomeId==",
-            avatarUrl: "https://avatars.githubusercontent.com/u/zzz?v=4",
-            login: "testuser-1",
-            name: "A User",
-            __typename: "User"
+            id: 'SomeId==',
+            avatarUrl: 'https://avatars.githubusercontent.com/u/zzz?v=4',
+            login: 'testuser-1',
+            name: 'A User',
+            __typename: 'User',
           },
-          __typename: "SearchResultItemEdge"
+          __typename: 'SearchResultItemEdge',
         },
         {
           node: {
-            id: "AnotherId==",
-            avatarUrl: "https://avatars.githubusercontent.com/u/yyyy?v=4",
-            login: "testuser-2",
-            name: "Another User",
-            __typename: "User"
+            id: 'AnotherId==',
+            avatarUrl: 'https://avatars.githubusercontent.com/u/yyyy?v=4',
+            login: 'testuser-2',
+            name: 'Another User',
+            __typename: 'User',
           },
-          __typename: "SearchResultItemEdge"
+          __typename: 'SearchResultItemEdge',
         },
         {
           node: {
-            id: "AThirdId==",
-            avatarUrl: "https://avatars.githubusercontent.com/u/xxxx?v=4",
-            login: "testuser-3",
-            name: "A Third User",
-            __typename: "User"
+            id: 'AThirdId==',
+            avatarUrl: 'https://avatars.githubusercontent.com/u/xxxx?v=4',
+            login: 'testuser-3',
+            name: 'A Third User',
+            __typename: 'User',
           },
-          __typename: "SearchResultItemEdge"
-        }
+          __typename: 'SearchResultItemEdge',
+        },
       ],
-      __typename: "SearchResultItemConnection"
-    }
+      __typename: 'SearchResultItemConnection',
+    },
   },
   loading: false,
   networkStatus: 7,
